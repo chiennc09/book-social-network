2a5e3f1bad38d9cfd2080ec427f13c4a9bb28b2c
unknown <chiennc09@gmail.com>
2025-09-16 16:20:56 +0700

    test kafka

--- identity-service/src/main/java/com/chiennc/identity/service/UserService.java
+++ identity-service/src/main/java/com/chiennc/identity/service/UserService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import com.chiennc.identity.mapper.ProfileMapper;
 import com.chiennc.identity.repository.httpclient.ProfileClient;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -40,6 +41,7 @@ public class UserService {
     PasswordEncoder passwordEncoder;
     ProfileMapper profileMapper;
     ProfileClient profileClient;
+    KafkaTemplate<String, String> kafkaTemplate;
 
     public UserResponse createUser(UserCreationRequest request) {
         if (userRepository.existsByUsername(request.getUsername())) throw new AppException(ErrorCode.USER_EXISTED);
@@ -57,6 +59,9 @@ public class UserService {
         profileRequest.setUserId(user.getId());
 
         profileClient.createProfile(profileRequest);
+
+        // Publish message to kafka
+        kafkaTemplate.send("onboard-successful", "Welcome our new member " + user.getUsername());
 
         return userMapper.toUserResponse(user);
     }
